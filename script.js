07cd995472e15deb9c7281593d0bac97c0698eac
fruitsrouges <justine.qu@outlook.fr>
2025-03-02 13:10:06 +0100

    main - maj burger menu

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 function toggleMenu() {
-    const nav = document.querySelector('.nav-bar');
+    const nav = document.querySelector('.nav-content');
+    const burger = document.querySelector('.burger');
+
     nav.classList.toggle('show');
+    burger.classList.toggle('active');
 }
